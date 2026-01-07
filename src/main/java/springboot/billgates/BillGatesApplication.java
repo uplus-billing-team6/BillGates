daac46b349ebd8ed53ae7b1dc78a1b6d4a0b5902
rladntlr <tlrrns08@gmail.com>
2026-01-07 14:12:54 +0900

    feat: 초기 프로젝트 생성

---
+++ src/main/java/springboot/billgates/BillGatesApplication.java
@@ -0,0 +1,13 @@
+package springboot.billgates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillGatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillGatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/billgates/BillGatesApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.billgates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillGatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
