cad903dc692fec2bd40790dff281cebcba232074
rladntlr <tlrrns08@gmail.com>
2026-01-13 11:48:02 +0900

    feat: #24 - kafka 이벤트 dto 생성 fix: #18 - 엔티티 char 변경, properties 추가, unique key 설정 추가

---
+++ src/main/java/springboot/billgates/dto/NotificationEvent.java
@@ -0,0 +1,27 @@
+package springboot.billgates.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEvent {
+
+    private Long messageId;
+
+    private Long memberId;
+
+    private Long billingId;
+
+    private String channel;
+
+    private String recipient;
+
+    private String emailTitle;
+
+    private String content;
+}
