a8f4ecd46b2900811c5f5ffe061eb67191d8517b
ChoiBoKeun1 <cbk121779@gmail.com>
2026-01-14 14:12:11 +0900

    feat: billing batch 임시 sql 쿼리 작성

--- src/main/java/springboot/billgates/domain/billing/sql/BillingSqls.java
+++ src/main/java/springboot/billgates/domain/billing/sql/BillingSqls.java
@@ -1,8 +1,24 @@
 package springboot.billgates.domain.billing.sql;
 
 public class BillingSqls {
-    public static final String SELECT_MEMBERS = "";
-    public static final String SELECT_USAGE_BY_MEMBER = "";
-    public static final String INSERT_BILLING = "";
-    public static final String INSERT_BILLING_ITEM = "";
+    public static final String SELECT_MEMBERS = "SELECT member_id, name, email, phone_number FROM MEMBER";
+    public static final String SELECT_USAGE_BY_MEMBER =
+        """
+        SELECT i.category, i.name , u.amount
+        FROM USAGE_HISTORY u
+        JOIN ITEM i ON u.item_id = i.item_id
+        WHERE u.member_id = ?
+          AND u.usage_date >= ?
+          AND u.usage_date < ?
+        """;
+    public static final String INSERT_BILLING =
+        """
+        INSERT INTO BILLING (member_id, billing_month, total_amount, created_at)
+        VALUES (?, ?, ?, ?)
+        """;
+    public static final String INSERT_BILLING_ITEM =
+        """
+        INSERT INTO BILLING_ITEM (billing_id, category, item_name, amount)
+        VALUES (?, ?, ?, ?)
+        """;
 }
