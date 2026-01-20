d755c38d561494ecd38c0e3f183611fdae5602dd
ChoiBoKeun1 <cbk121779@gmail.com>
2026-01-20 16:32:05 +0900

    fix: 중복 repository 주석 처리

--- src/main/java/springboot/billgates/domain/member/repository/MemberRepository.java
+++ src/main/java/springboot/billgates/domain/member/repository/MemberRepository.java
@@ -1,8 +1,8 @@
-package springboot.billgates.domain.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springboot.billgates.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
+//package springboot.billgates.domain.member.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import springboot.billgates.entity.Member;
+//
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//
+//}
