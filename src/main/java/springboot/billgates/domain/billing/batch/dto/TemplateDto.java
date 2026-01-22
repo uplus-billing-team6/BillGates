2dde50eaaa8e77de97b76771f00b1ae0a73b97a9
ChoiBoKeun1 <cbk121779@gmail.com>
2026-01-22 11:49:55 +0900

    feat: message_template 유틸메서드 수정

--- src/main/java/springboot/billgates/domain/billing/batch/dto/TemplateDto.java
+++ src/main/java/springboot/billgates/domain/billing/batch/dto/TemplateDto.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class TemplateDto {
+    private final String channel;
     private final String title;
     private final String body;
 }
