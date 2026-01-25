fddfd17869c7b1bf590d96c7f0e19c08d1442007
ljh5918 <akwogur123@naver.com>
2026-01-25 17:26:31 +0900

    feat: #91 프론트 연동

--- src/main/java/springboot/billgates/repository/MessageTemplateRepository.java
+++ src/main/java/springboot/billgates/repository/MessageTemplateRepository.java
@@ -5,6 +5,5 @@ import springboot.billgates.entity.MessageTemplate;
 import java.util.Optional;
 
 public interface MessageTemplateRepository extends JpaRepository<MessageTemplate, Long> {
-    // 필드명이 channel이므로 findByChannel로 작성해야 합니다.
     Optional<MessageTemplate> findByChannel(String channel);
 }
